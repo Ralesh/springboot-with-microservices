8351feef06e159db44574758ee106000aede1439
Ralesh Biswal <mymac@Raleshs-MacBook-Air.local>
2020-07-15 10:55:45 +0530

    netflix-eureka-server

---
+++ src/main/java/com/springboot/rest/webservices/restfulwebservices/netflixeurekanamingserver/NetflixEurekaNamingServerApplication.java
@@ -0,0 +1,18 @@
+package com.springboot.rest.webservices.restfulwebservices.netflixeurekanamingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+/*
+ * Eureka Naming Server 
+ * Video - 87 to 96 (Ribbon load balancing to eureka naming server)
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class NetflixEurekaNamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixEurekaNamingServerApplication.class, args);
+	}
+
+}
